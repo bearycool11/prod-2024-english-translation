66000ef98e9fc074c81caefa86d20b9d25ad9a18
Morzan6 <morzann09@gmail.com>
2024-03-31 00:04:59 +0300

    cleaned and flowbite added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
+import './styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
+
 
 const app = createApp(App)
 
