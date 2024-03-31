eda20b39f83ca62eed28ba5945a4390370af0ea2
fede4ka1245 <bragin.fedor96@gmail.com>
2024-03-31 18:12:40 +0500

    commit changes: - moved Login.vue - fix button MainLayout.vue

--- frontend/src/pages/MainLayout.vue
+++ frontend/src/pages/MainLayout.vue
@@ -5,9 +5,7 @@
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <a class="flex items-center font-medium text-xl"> Username </a>
         <div class="flex items-center lg:order-2">
-          <button
-            class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-          >
+          <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
             Add org
           </button>
         </div>
