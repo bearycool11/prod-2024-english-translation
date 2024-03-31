43b70f7ea8da0a92b8424bb601a902f45a3d4850
Morzan6 <morzann09@gmail.com>
2024-03-31 13:17:34 +0300

    minor fixes 	modified:   docker-compose.yml 	modified:   frontend/src/App.vue 	renamed:    nginx-conf.d/nginx.conf -> nginx/nginx.conf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { FwbButton } from 'flowbite-vue'
 </script>
 
 <template>
   <header>
+    <FwbButton color="green">GG</FwbButton>
     header main (username+ add org)
   </header>
   <RouterView />
