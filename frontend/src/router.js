252af31e994dcdece5be8d79f76f3f4efcb6b14b
fede4ka1245 <bragin.fedor96@gmail.com>
2024-03-31 17:49:12 +0500

    commit changes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -12,16 +12,6 @@ const router = createRouter({
           name: 'OrganizationPage',
           //redirect: {name: 'posts'},
           children: [
-            // {
-            //   path: '/',
-            //   name: 'postsr',
-            //   redirect: {name: 'posts'},
-            // },
-            {
-              path: '/posts',
-              name: 'posts',
-              component: () => import("./pages/TestPage.vue")
-            },
             {
               path: '/users',
               name: 'users',
