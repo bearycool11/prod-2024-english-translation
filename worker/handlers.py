45aa3a22a28e119005362f43df8c8a66989b7248
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-01 17:06:00 +0300

    add handler for sending message

--- worker/handlers.py
+++ worker/handlers.py
@@ -1,1 +1,14 @@
+import telebot
+
 from utils import task
+
+
+@task
+def send_message(bot_token, channel_id, message_text, post_id):
+    bot = telebot.TeleBot(bot_token)
+    try:
+        bot.send_message(chat_id=channel_id, text=message_text, parse_mode="MarkdownV2")
+        # ok
+    except:
+        # error
+        pass
