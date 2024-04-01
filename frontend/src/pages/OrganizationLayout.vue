a1bdaf3185322bdebb7db27acac2f65b8048c35e
Morzan6 <morzann09@gmail.com>
2024-04-01 13:49:24 +0300

    users url fixed 	modified:   frontend/src/pages/OrganizationLayout.vue

--- frontend/src/pages/OrganizationLayout.vue
+++ frontend/src/pages/OrganizationLayout.vue
@@ -37,7 +37,7 @@
         <li>
           <a
           v-if="users"
-            :href="$route.fullPath + '/users'"
+            :href="`/organization/${id}/users`"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
           >
             <svg
