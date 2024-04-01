040768a0f41eb2e797ae874d7437ac05f13e37a0
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-02 00:29:27 +0300

    add private api calls to worker

--- worker/handlers.py
+++ worker/handlers.py
@@ -1,3 +1,4 @@
+import requests
 import telebot
 
 from utils import task
@@ -8,7 +9,8 @@ def send_message(bot_token, channel_id, message_text, post_id):
     bot = telebot.TeleBot(bot_token)
     try:
         bot.send_message(chat_id=channel_id, text=message_text, parse_mode="MarkdownV2")
-        # ok
+        requests.post("http://smm_ya_backend:5437/api/private/set_post_sent_state",
+                      json={"post_id": post_id, "post_status": "SENT_OK"})
     except:
-        # error
-        pass
+        requests.post("http://smm_ya_backend:5437/api/private/set_post_sent_state",
+                      json={"post_id": post_id, "post_status": "SENT_ERROR"})
