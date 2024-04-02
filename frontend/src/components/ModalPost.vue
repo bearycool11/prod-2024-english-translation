7ff368897d99716648445122f2426d860a342b40
Morzan6 <morzann09@gmail.com>
2024-04-02 11:05:08 +0300

    start posts schedule 	modified:   frontend/src/components/ModalPost.vue

--- frontend/src/components/ModalPost.vue
+++ frontend/src/components/ModalPost.vue
@@ -56,7 +56,23 @@
               />
               {{ text }}
             </div>
+            <div class="col-span-2" v-if="!this.creation && this.content.is_approved === 'APPROVED'">
+              <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Запланировать</label
+              >
+              <input
+                type="text"
+                name="name"
+                id="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Запланировать"
+                required
+                
+              />
+              {{ text }}
+            </div>
           </div>
+          <div class="flex justify-between">
           <button
             @click="addChannel"
             type="submit"
@@ -76,10 +92,20 @@
             </svg>
             {{ buttonText }}
           </button>
+          <button
+            
+            type="submit"
+            class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+        
+            Отправить    
+        </button>
+        </div>
         </form>
       </div>
     </div>
   </div>
+  {{ content }}
 </template>
 <script>
 import { defineComponent } from 'vue'
