4385ac939255dbabca33c14affe65235f5602936
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-02 19:36:05 +0300

    fix task kwarg issue

--- worker/utils.py
+++ worker/utils.py
@@ -6,8 +6,8 @@ def task(func):
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        func_res = func(*args, **kwargs)
         task_model = kwargs.pop("task")
+        func_res = func(*args, **kwargs)
         with get_session() as session:
             session.delete(task_model)
             session.commit()
