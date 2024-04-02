a1019fee7ec1e8d51a6f592d163e13bb97df7a9e
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-02 16:26:20 +0300

    updated_at is optional

--- backend/models.py
+++ backend/models.py
@@ -155,7 +155,7 @@ class Post(BaseModel):
     sent_status: SentStatus
     channels: list[Channel]
     tags: list[str]
-    updated_at: datetime.datetime
+    updated_at: Optional[datetime.datetime]
 
 
 class GetPostsResponse(BaseModel):
