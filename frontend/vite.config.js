74396651940e720133ee542320d15059cfc77c9f
fede4ka1245 <bragin.fedor96@gmail.com>
2024-04-03 00:44:09 +0500

    fix port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { host: 'test-domain.ru', port: 80 }, // Not needed for Vite 5+
+  server: {
+    host: '0.0.0.0',
+    port: 8888
+  },
   plugins: [
-    vue()
+    vue(),
   ],
   resolve: {
     alias: {
