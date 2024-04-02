f70da7dbe459c1eb14072f65a0a73e74b6fe2455
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-02 20:43:12 +0300

    fix try-except

--- worker/handlers.py
+++ worker/handlers.py
@@ -10,8 +10,8 @@ from utils import task
 
 @task
 def send_message(bot_token, channel_id, telegram_channel_id, message_text, post_id):
-    bot = telebot.TeleBot(bot_token)
     try:
+        bot = telebot.TeleBot(bot_token)
         message = bot.send_message(chat_id=telegram_channel_id, text=message_text)
         requests.post("http://smm_ya_backend:5437/api/private/set_post_sent_state",
                       json={"post_id": post_id, "post_status": "SENT_OK", "telegram_message_id": message.message_id,
