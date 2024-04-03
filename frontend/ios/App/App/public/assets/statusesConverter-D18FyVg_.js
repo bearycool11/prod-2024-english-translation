2411132ffc67af8babef925de83691e16c85fc6e
fede4ka1245 <bragin.fedor96@gmail.com>
2024-04-03 10:38:28 +0500

    add ios clone

---
+++ frontend/ios/App/App/public/assets/statusesConverter-D18FyVg_.js
@@ -0,0 +1,1 @@
+function r(e){switch(e){case"WAITING":return"ОЖИДАЕТ";case"REJECTED":return"ОТКЛОНЁН";case"APPROVED":return"ПОДТВЕРЖДЕН";case"OPEN":return"ОТКРЫТ";case"NOT_READY":return"НЕ ГОТОВ";case"SENT_OK":return"ОТПРАВЛЕН"}}export{r as c};
