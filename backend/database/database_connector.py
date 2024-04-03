e1150ca70b14cf29498e7ee6606f5ff20f2319f2
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-04-03 08:28:28 +0300

    return database

--- backend/database/database_connector.py
+++ backend/database/database_connector.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
 import config
 
@@ -7,6 +8,24 @@ engine = create_engine(config.database_url, echo=False)
 session_maker = sessionmaker(engine, expire_on_commit=False)
 
 
+def load_models():
+    from database.models import DBUser  # noqa: unused
+    from database.models import DBOrganization  # noqa: unused
+    from database.models import DBPermission  # noqa: unused
+    from database.models import DBOrganizationUser  # noqa: unused
+    from database.models import DBOrganizationBot  # noqa: unused
+    from database.models import DBChannel  # noqa: unused
+    from database.models import DBPost  # noqa: unused
+    from database.models import DBTask  # noqa: unused
+    from database.models import post_channel_bindings  # noqa: unused
+
+
 def get_session():
     with session_maker() as session:
         yield session
+
+
+def init_models() -> None:
+    load_models()
+    with engine.begin() as conn:
+        SQLModel.metadata.create_all(conn)
